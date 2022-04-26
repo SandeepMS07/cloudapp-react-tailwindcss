5a0c82f6b0fb2aa9a1e66a9cf2644740c150e4da
SandeepMS07 <sandeepms746@gmail.com>
2022-04-26 18:19:30 +0530

    src code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Trusted by developers across the world</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+            qui aperiam dicta consequatur voluptate magnam autem earum soluta
+            fuga. Corrupti!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
